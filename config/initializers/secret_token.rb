3c61bcd887646291183b8bea27e41ce4603564cf
Francesc Pellicero <pelly.obn91@gmail.com>
2012-10-28 16:21:18 +0100

    Aplicació Rails creada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerBandaPanda::Application.config.secret_token = 'c6b005f777ce6f475ff1ddc25e802a6737eaef93b6cffabcfb79c4fcd255a0e240b6040555a5316b14fd59e5611d37b3427a51fabbd9165175eb3463f72c7259'
