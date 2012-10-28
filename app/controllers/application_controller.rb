2b98f61a46659e429db8320c2f9b6d07f09fe5c7
Francesc Pellicero <pelly.obn91@gmail.com>
2012-10-28 18:57:53 +0100

    Token pot ser passat com a parametre ?auth_token o al header X-AUTH-TOKEN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+  # Abans d'autenticar, ens assegurem de que el token estigui on volem
+  prepend_before_filter :get_auth_token
+  # Abans d'executar res, autentiquem l'usuari
+  before_filter :authenticate_user!
+  
   protect_from_forgery
+
+  # En cas de que no passem un auth_token com a parametre però si com a Header, copiem el token a l'array de params perque aixi ho demana devise.
+  private
+  def get_auth_token
+  	if auth_token = params[:auth_token].blank? && request.headers["X-AUTH-TOKEN"]
+  		params[:auth_token] = auth_token
+  	end
+  end
 end
