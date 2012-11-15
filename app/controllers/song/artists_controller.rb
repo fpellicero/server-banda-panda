53042b44deb2178213d0c0db96ad0c84e932fbc7
Albert <nuxe@nuxe-VPCYB3V1E.(none)>
2012-11-15 13:47:27 +0100

    moguts controllers de cancons a song/

---
+++ app/controllers/song/artists_controller.rb
@@ -0,0 +1,35 @@
+class ArtistsController < ApplicationController
+  # GET /artists
+  # GET /artists.json
+  def index
+    @artists = Artist.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @artists }
+    end
+  end
+
+  def new
+    @artist = Artist.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @artist }
+    end
+  end
+
+  def create
+    @artist = Artist.new(params[:artist])
+
+    respond_to do |format|
+      if @artist.save
+        format.html { redirect_to @artist, notice: 'artist was successfully created.' }
+        format.json { render json: @artist, status: :created, location: @artist }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
---
+++ app/controllers/song/songs_controller.rb
@@ -0,0 +1,86 @@
+class SongsController < ApplicationController
+  # GET /songs
+  # GET /songs.json
+  def index
+    @songs = Song.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @songs }
+    end
+  end
+
+  # GET /songs/1/edit
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  # GET /songs/new
+  # GET /songs/new.json
+  def new
+    @song = Song.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @song }
+    end
+  end
+
+  # POST /songs
+  # POST /songs.json
+  def create
+    @song = Song.new(params[:song])
+
+    respond_to do |format|
+      if @song.save
+        format.html { redirect_to @song, notice: 'Song was successfully created.' }
+        format.json { render json: @song, status: :created, location: @song }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /songs/1
+  # PUT /songs/1.json
+  def update
+    @song = Song.find(params[:id])
+
+    respond_to do |format|
+      if @song.update_attributes(params[:song])
+        format.html { redirect_to @song, notice: 'Song was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /songs/1
+  # DELETE /songs/1.json
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+
+    respond_to do |format|
+      format.html { redirect_to songs_url }
+      format.json { head :no_content }
+    end
+  end
+
+  #GET /songs/search?q=”...”&order=”...”&lim=”...”&offset=”...”
+  def search
+    @songs = Song.where("title LIKE ?", "%#{params[:q]}%").limit(params[:lim]).offset(params[:offset]).order("title ?", params[:order])
+    results = Array.new()
+    @songs.each do |s|
+      result = {:song_id => s.id, :song_title => s.title, :album_id => s.album_id, :album_title => s.album.title,
+                   :artist_id => s.album.artist_id, :artist_name => s.album.artist.name, :audio_url => s.url, :cover_url => s.album.cover}
+      results.push(result)
+    end
+    respond_to do |format|
+      format.json { render json: results }
+    end
+  end
+end
