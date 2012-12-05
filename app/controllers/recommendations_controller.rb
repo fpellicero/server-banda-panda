c983ac0d44e0d9e4b4c27d5b4dd6158b5d8ae1f5
Albert Millas <millas@gmail.com>
2012-12-05 17:13:30 +0100

    affegits errors a recommendations#get

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -51,23 +51,61 @@ class RecommendationsController < ApplicationController
 	#GET /users/{id}/recommendations
 	def get
 		results = Array.new()
+		lim = 30
+   		offset = 0
 		status = 200
-		recommendations = Recommendation.where("target_id = ?", "#{params[:id]}")
 
-		recommendations.each do |r|
-			result = {:source_id => r.source_id, :type => r.type, :resource_id => r.resource_id, :date => r.created_at, :read => r.read}
-			# si encara no ha estat llegida, la marquem com a llegida.
-			if r.read == 0
-				r.update_attribute :read, 1
+		#comprovem lim
+		if params[:lim] 
+	      if !params[:lim].is_i? || !(Integer(params[:lim]) > 0) 
+	        status = 400
+	      else
+	        lim = params[:lim]
+	      end
+	    end
+	    #comprovem offset
+	    if params[:offset] 
+	      if !params[:offset].is_i? || !(Integer(params[:offset]) >= 0) 
+	        status = 400
+	      else
+	        offset = params[:offset]
+	      end
+	    end
+
+	   	if !User.exists?(params[:id])
+		  status = 404
+		end
+
+		if status == 200
+			recommendations = Recommendation.where("target_id = ?", "#{params[:id]}")
+
+			recommendations.each do |r|
+				result = {:source_id => r.source_id, :type => r.type, :resource_id => r.resource_id, :date => r.created_at, :read => r.read}
+				# si encara no ha estat llegida, la marquem com a llegida.
+				if r.read == 0
+					r.update_attribute :read, 1
+				end
+				results.push(result)
 			end
-			results.push(result)
+	      	filtered_results = results[Integer(offset)..(Integer(offset)+Integer(lim)-1)] #Apliquem offsets i limits
+	      	if Integer(offset) >= results.size() && Integer(offset) != 0
+	        	status = 416 #Offset fora de arxiu
+	      	elsif results[Integer(offset)+Integer(lim)]
+	        	status = 206 #Partial Content
+	      	end
 		end
 
 		respond_to do |format|
 	  		unless format.json 
 		        {:status => 406} #Nomes retorna Json
 		    end
-		    format.json { render json: results, :status => status }
+		    format.json { render json: filtered_results, :status => status }
 		end
 	end
+end
+
+class String
+    def is_i?
+       !!(self =~ /^[-+]?[0-9]+$/)
+    end
 end
